4ef2b53b65b7657f819141bf496afe09085c97f6
CMusger <it.musger@gmail.com>
2022-09-27 14:30:56 +0200

    Removed Array.From(..)

--- Intermediate/JS/main.js
+++ Intermediate/JS/main.js
@@ -1,11 +1,10 @@
-// Convert NodeList/HTML Collection into Array
-let santa = Array.from(document.getElementsByClassName("santa"));
+let santa = document.getElementsByClassName("santa");
 
 const hide = () => {
     for(let i in santa){
         santa[i].addEventListener("click",() =>{
             santa[i].style.opacity ="0";
-            
+
             document.getElementById("start").style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
         });
     }
